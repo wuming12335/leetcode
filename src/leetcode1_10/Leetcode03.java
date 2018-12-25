344fc350c0387e4d1666808c9f1b5cd2fd8e5dee
wuming12335 <499707547@qq.com>
2018-12-25 10:06:19 +0800

    无重复字符的最长子串

--- src/leetcode1_10/Leetcode03.java
+++ src/leetcode1_10/Leetcode03.java
@@ -7,6 +7,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/*
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+*/
+
 public class Leetcode03 {
 	public static int lengthOfLongestSubstring(String s) {
 		
