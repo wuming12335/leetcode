6c25e83a89a033bdb29600e9633ac8497746d8f3
wuming12335 <499707547@qq.com>
2019-02-21 10:14:02 +0800

    69.x 的平方根

---
+++ src/leetcode1_10/Leetcode69.java
@@ -0,0 +1,61 @@
+package leetcode1_10;
+
+//69. x 的平方根
+/*
+实现 int sqrt(int x) 函数。
+
+计算并返回 x 的平方根，其中 x 是非负整数。
+
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+示例 1:
+
+输入: 4
+输出: 2
+示例 2:
+
+输入: 8
+输出: 2
+说明: 8 的平方根是 2.82842..., 
+     由于返回类型是整数，小数部分将被舍去。
+*/
+public class Leetcode69 {
+	public static int mySqrt(int x) {
+		
+		long t = x;
+		t = 0x5f3759df - (t >> 1);
+		while (!(t*t <= x && (t+1)*(t+1) > x))
+			t = (x/t + t)/2;
+		return (int)t;
+
+	}
+	public static int mySqrt2(int x) {
+		
+		int ans = 1;
+        if (0 == x){
+            return 0;
+        }
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		int x = 2147395599;
+		
+		int mySqrt = mySqrt2(x);
+		System.out.println(mySqrt);
+	}
+	
+}
